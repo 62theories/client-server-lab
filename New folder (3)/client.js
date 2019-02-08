fdf138b1397ab55d8d900da8f8dda0fb035594e5
62theories <33127512+62theories@users.noreply.github.com>
2019-02-08 14:27:05 +0700

    Update client.js

--- New folder (3)/client.js
+++ New folder (3)/client.js
@@ -1,9 +1,9 @@
 var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 6969;
-var count = 0
+var that = ""
 var client = new net.Socket();
-var stop = 0
+
 
 
 client.connect(PORT, HOST, function() {
@@ -13,25 +13,7 @@ client.connect(PORT, HOST, function() {
 
 client.on('data', function(data) {
    console.log('server said: ' + data);
-
-	let stdin = process.openStdin()
-   if(data == 'OK' || data == 'WRONG')
-   {
-   	stdin.addListener("data", (value) =>{
-	input = value.toString().trim()
-	client.write(input);
-	
-	})	
-   }
-   else if(data == 'BINGO')
-   {
-   	
-   }
-
-   
-   
-  
-
+   that = data
 
    
 });
@@ -41,3 +23,18 @@ client.on('close', function() {
    console.log('Connection closed');
    client.destroy();
 });
+
+
+let stdin = process.openStdin()
+  
+      stdin.addListener("data", (value) =>{
+      input = value.toString().trim()
+     if(that == 'OK' || that == 'WRONG')
+     {
+      client.write(input)
+     }
+     else if(that == 'BINGO')
+     {
+      client.destroy()
+     }
+   })

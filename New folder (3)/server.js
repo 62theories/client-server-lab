13a416d7566cc9ca567bb9cc313464f359b568a2
62theories <33127512+62theories@users.noreply.github.com>
2019-02-08 14:26:37 +0700

    Update server.js

--- New folder (3)/server.js
+++ New folder (3)/server.js
@@ -3,33 +3,32 @@ var count = 0
 var HOST = '127.0.0.1';
 var PORT = 6969;
 var name = ""
-var answer = 6
+var answer=Math.floor(Math.random() * 21)
+console.log('answer is ' + answer.toString())
 net.createServer(function(sock) {
    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
    sock.on('data', function(data) {
+         console.log('client said: ' + data);
    		if(count == 0)
    		{
    			name = data
    			sock.write('OK');
    			count++
    		}
-   		else if(data == answer)
+   		else if(data == answer.toString())
    		{
    			sock.write('BINGO');
    			sock.end()
    		}
-   		else if(count == 6)
+   		else if(count == 5)
    		{
+            sock.write('END');
    			sock.end()
-   		}
-   		else if(data == 'cleared')
-   		{
-
+            sock.destroy()
    		}
    		else
    		{
    			sock.write('WRONG');
-   			data = 'cleared'
    			count++
    		}
 
